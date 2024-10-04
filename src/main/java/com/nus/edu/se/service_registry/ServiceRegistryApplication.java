d84b9a27d79b558db25301adac12d676419c5f75
Nicholas Yuen <e1337517@u.nus.edu>
2024-10-04 23:57:32 +0800

    revert serviceregistry

--- src/main/java/com/nus/edu/se/service_registry/ServiceRegistryApplication.java
+++ src/main/java/com/nus/edu/se/service_registry/ServiceRegistryApplication.java
@@ -3,9 +3,11 @@ package com.nus.edu.se.service_registry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import com.netflix.appinfo.AmazonInfo;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
 import org.springframework.cloud.commons.util.InetUtils;
-
 
 @SpringBootApplication
 @EnableEurekaServer
@@ -15,13 +17,15 @@ public class ServiceRegistryApplication {
 		SpringApplication.run(ServiceRegistryApplication.class, args);
 	}
 
-	@Bean
-	@Profile("!default")
-	public EurekaInstanceConfigBean eurekaInstanceConfig(InetUtils inetUtils) {
-	EurekaInstanceConfigBean b = new EurekaInstanceConfigBean(inetUtils);
-	AmazonInfo info = AmazonInfo.Builder.newBuilder().autoBuild("eureka");
-	b.setDataCenterInfo(info);
-	return b;
-	}
 
 }
+
+//@Bean
+//@Profile("!default")
+//public EurekaInstanceConfigBean eurekaInstanceConfig(InetUtils inetUtils) {
+//	EurekaInstanceConfigBean eurekaInstanceConfigBean = new EurekaInstanceConfigBean(inetUtils);
+//	AmazonInfo amazonInfo = AmazonInfo.Builder.newBuilder().autoBuild("eureka");
+//	eurekaInstanceConfigBean.setDataCenterInfo(amazonInfo);
+//	return eurekaInstanceConfigBean;
+//}
+//}
