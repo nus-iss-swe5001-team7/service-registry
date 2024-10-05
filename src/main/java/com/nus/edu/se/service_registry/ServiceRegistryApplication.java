ca12cb4d23203982e89b6cbb178d88c31a39b4b6
Nicholas Yuen <e1337517@u.nus.edu>
2024-10-05 19:41:43 +0800

    trying to revert service registry

--- src/main/java/com/nus/edu/se/service_registry/ServiceRegistryApplication.java
+++ src/main/java/com/nus/edu/se/service_registry/ServiceRegistryApplication.java
@@ -3,11 +3,6 @@ package com.nus.edu.se.service_registry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Profile;
-import com.netflix.appinfo.AmazonInfo;
-import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
-import org.springframework.cloud.commons.util.InetUtils;
 
 @SpringBootApplication
 @EnableEurekaServer
@@ -17,15 +12,4 @@ public class ServiceRegistryApplication {
 		SpringApplication.run(ServiceRegistryApplication.class, args);
 	}
 
-
 }
-
-//@Bean
-//@Profile("!default")
-//public EurekaInstanceConfigBean eurekaInstanceConfig(InetUtils inetUtils) {
-//	EurekaInstanceConfigBean eurekaInstanceConfigBean = new EurekaInstanceConfigBean(inetUtils);
-//	AmazonInfo amazonInfo = AmazonInfo.Builder.newBuilder().autoBuild("eureka");
-//	eurekaInstanceConfigBean.setDataCenterInfo(amazonInfo);
-//	return eurekaInstanceConfigBean;
-//}
-//}
