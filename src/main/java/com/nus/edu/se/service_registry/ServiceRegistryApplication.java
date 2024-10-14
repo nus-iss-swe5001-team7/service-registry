c87ec478fe0423925ee58e807d47f81670a32bd2
Nicholas Yuen <e1337517@u.nus.edu>
2024-10-14 23:17:46 +0800

    update

--- src/main/java/com/nus/edu/se/service_registry/ServiceRegistryApplication.java
+++ src/main/java/com/nus/edu/se/service_registry/ServiceRegistryApplication.java
@@ -4,6 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import com.netflix.appinfo.AmazonInfo;
+import com.netflix.appinfo.EurekaInstanceConfig;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
+import org.springframework.cloud.commons.util.InetUtils;
+
+
 @SpringBootApplication
 @EnableEurekaServer
 public class ServiceRegistryApplication {
